99766f97fc4f3fd03dbc3e47e995a067f51dfa5d
Cristhian Hoyos <89754731+choyos1@users.noreply.github.com>
2022-06-28 22:57:01 -0400

    Update Projectile.cs

--- scripts/Projectile.cs
+++ scripts/Projectile.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Projectile : MonoBehaviour
 {
-    public float speed = 20f;
+    public float Speed = 20f;
     public Vector3 direction = Vector3.up;
     public System.Action<Projectile> destroyed;
     public new BoxCollider2D collider{ get; private set; }
@@ -13,35 +13,30 @@ public class Projectile : MonoBehaviour
     {
         collider = GetComponent<BoxCollider2D>();
     }
-
-    private void OnDestroy()
+   
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        CheckCollision(other);
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        CheckCollision(other);
+    }
+     private void IfDestroy()
     {
         if (destroyed != null) {
             destroyed.Invoke(this);
         }
     }
-
     private void Update()
     {
         transform.position += direction * speed * Time.deltaTime;
     }
-
     private void CheckCollision(Collider2D other)
     {
         Sheild sheilds = other.gameObject.GetComponent<Sheild>();
 
             Destroy(gameObject);
         }
-    
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        CheckCollision(other);
-    }
-
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        CheckCollision(other);
-    }
-
+ 
 }
