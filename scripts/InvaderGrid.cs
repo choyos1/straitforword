a832dd2f55b002ecf4d288679b891bb383a6d736
Cristhian Hoyos <89754731+choyos1@users.noreply.github.com>
2022-06-28 22:53:28 -0400

    Update and rename Invaders.cs to InvaderGrid.cs

--- scripts/InvaderGrid.cs
+++ scripts/InvaderGrid.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class Invaders : MonoBehaviour
 {
-   
     public Invader[] PreFab = new Invader[5];
     public AnimationCurve Speed = new AnimationCurve();
     public Vector3 Direction { get; private set; } = Vector3.right;
@@ -13,29 +12,22 @@ public class Invaders : MonoBehaviour
     public int InvadersLeft => TotalInvaders - InvaderDestroyed;
     public int TotalInvaders => Row * Col;
     public float PercentDestroyed => (float)InvaderDestroyed / (float)TotalInvaders;
-
-
     public int Row = 5;
     public int Col = 11;
-
     public Projectile MisslePreFab;
     public float MissleSpawnRate = 1f;
 
     private void Awake()
     {
         StartingSpot = transform.position;
-
         for (int i = 0; i < Row; i++)
         {
             float width = 2f * (Col - 1);
             float height = 2f * (Row - 1);
-
             Vector2 centerOffset = new Vector2(-width * 1f, -height * 1f);
             Vector3 rowLocation = new Vector3(centerOffset.x, (2f * i) + centerOffset.y, 0f);
-
             for (int j = 0; j < Col; j++)
             {
-
                 Invader invader = Instantiate(PreFab[i], transform);
                 invader.killed += OnInvaderDestroyed;
                 Vector3 position = rowLocation;
@@ -44,12 +36,10 @@ public class Invaders : MonoBehaviour
             }
         }
     }
-
     private void Start()
     {
         InvokeRepeating(nameof(MissleDrop), MissleSpawnRate, MissleSpawnRate);
     }
-
     private void MissleDrop()
     {
         int Straglers = InvadersLeft;
@@ -58,14 +48,12 @@ public class Invaders : MonoBehaviour
         {
             return;
         }
-
         foreach (Transform invader in transform)
         {
             if (!invader.gameObject.activeInHierarchy)
             {
                 continue;
             }
-
             if (Random.value < (1f / (float)Straglers))
             {
                 Instantiate(MisslePreFab, invader.position, Quaternion.identity);
@@ -73,25 +61,18 @@ public class Invaders : MonoBehaviour
             }
         }
     }
-
     private void Update()
     {
         float Speed = this.Speed.Evaluate(PercentDestroyed);
         transform.position += Direction * Speed * Time.deltaTime;
-
         Vector3 leftSide = Camera.main.ViewportToWorldPoint(Vector3.zero);
         Vector3 rightSide = Camera.main.ViewportToWorldPoint(Vector3.right);
-
-
         foreach (Transform invader in transform)
         {
-
             if (!invader.gameObject.activeInHierarchy)
             {
                 continue;
             }
-
-
             if (Direction == Vector3.right && invader.position.x >= (rightSide.x - 1f))
             {
                 AdvanceRow();
@@ -104,18 +85,13 @@ public class Invaders : MonoBehaviour
             }
         }
     }
-
     private void AdvanceRow()
     {
-
         Direction = new Vector3(-Direction.x, 0f, 0f);
-
-
         Vector3 position = transform.position;
         position.y -= 1f;
         transform.position = position;
     }
-
     private void OnInvaderDestroyed(Invader invader)
     {
         invader.gameObject.SetActive(false);
@@ -126,8 +102,6 @@ public class Invaders : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-
-
     }
 
     public void ResetInvaders()
@@ -141,6 +115,4 @@ public class Invaders : MonoBehaviour
             invader.gameObject.SetActive(true);
         }
     }
-
-
 }
